ffd7e51be3f7b46d3d74be4f6d0d66656651ceb0
septianPadli <muhammadpadliseptiana@gmail.com>
2022-11-27 06:28:29 +0700

    Add Show Password
    Add function for show/hide password

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,44 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    margin:16,
-    nav:false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        768:{
-            items:2
-        },
-        992:{
-            items:3
-        },
-        1400:{
-            items:4
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:16,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1400:{
+                items:4
+            }
         }
-    }
-  });
-  $(".owl-carousel").on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
+    });
+    $(".owl-carousel").on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+
+    // Password Visibility
+    $("body").on('click', '.toggle-password', function() {
+        $(this).toggleClass("bi-eye bi-eye-slash");
+        var input = $("#password");
+        console.log(input)
+        input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+    });
+    $("body").on('click', '.toggle-confpassword', function() {
+        $(this).toggleClass("bi-eye bi-eye-slash");
+        var input = $("#confpassword");
+        console.log(input)
+        input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+    });
 });
